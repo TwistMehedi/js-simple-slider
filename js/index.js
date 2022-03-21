6d743c9abf9f966e1080ef2f80157182a3c97f14
TwistMehedi <ataul1708.com>
2022-03-21 23:07:42 +0600

    simple slider

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const slideShowElements = document.querySelectorAll(".slideshow-element");
+// console.log(slideShowElements)
+
+let countElements = 1;
+
+setInterval(() => {
+    countElements++;
+    let currentElement = document.querySelector(".current")
+    currentElement.classList.remove("current")
+    if (countElements > slideShowElements.length) {
+        slideShowElements[0].classList.add("current")
+    }
+    else {
+        currentElement.nextElementSibling.classList.add("current");
+    }
+
+}, 2000)
